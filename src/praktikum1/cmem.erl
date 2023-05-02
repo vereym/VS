11d7278cb500298639e604cc0df0e12c06261af7
infwps562 <benjamin.schroeder@haw-hamburg.de>
2023-05-02 20:17:54 +0200

    add cmem implementation

--- src/praktikum1/cmem.erl
+++ src/praktikum1/cmem.erl
@@ -1,5 +1,76 @@
 -module(cmem).
--export([initCMEM/2]).
 
-initCMEM(_, _) ->
-  ok.
+-export([initCMEM/2, delCMEM/1, updateClient/4, getClientNNr/2, listCMEM/1, lengthCMEM/1]).
+
+-import(util, [logging/2, writelist/2, randomliste/3]).
+-import(vsutil, [get_config_value/2, now2string/1, now2stringD/1, meinSleep/1, validTS/1, diffTS/2]).
+-import(io_lib, [format/2]).
+
+initCMEM(RemTime, LogFile) ->
+    logging(
+        LogFile,
+        format("~s: CMEM erfolgreich initialisiert~n", [
+            now2string(erlang:timestamp())
+        ])
+    ),
+    {RemTime, []}.
+
+delCMEM(_CMEM) ->
+    ok.
+
+updateClient(CMEM, ClientID, NNr, LogFile) ->
+    LastInteraction = erlang:timestamp(),
+    NewCMEM = forgetClients(CMEM),
+    {RemTime, ClientList} = NewCMEM,
+    NewClientList = updateClientList(ClientList, ClientID, LastInteraction, NNr),
+    logging(
+        LogFile,
+        format("~s: CMEM erfolgreich aktualisiert~n", [
+            now2string(erlang:timestamp())
+        ])
+    ),
+    {RemTime, NewClientList}.
+
+updateClientList([], ClientID, LastInteraction, NNr) ->
+    [{ClientID, LastInteraction, NNr}];
+updateClientList([{ClientID, _, _} | T], ClientID, LastInteraction, NNr) ->
+    [{ClientID, LastInteraction, NNr} | T];
+updateClientList([H | T], ClientID, LastInteraction, NNr) ->
+    [H | updateClientList(T, ClientID, LastInteraction, NNr)].
+
+forgetClients({RemTime, ClientList}) ->
+    Now = erlang:timestamp(),
+    NewClientList = forgetClients(ClientList, Now, RemTime),
+    {RemTime, NewClientList}.
+
+forgetClients([], _Now, _RemTime) ->
+    [];
+forgetClients([H = {_ID, LastInteraction, _NNr} | T], Now, RemTime) ->
+    TimeDifference = diffTS(Now, LastInteraction),
+    if
+        TimeDifference > RemTime ->
+            [forgetClients(T, Now, RemTime)];
+        true ->
+            [H | forgetClients(T, Now, RemTime)]
+    end.
+
+getClientNNr({_RemTime, ClientList}, ClientID) ->
+    getClientNNrInternal(ClientList, ClientID).
+
+getClientNNrInternal([], _ClientID) ->
+    1;
+getClientNNrInternal([{ClientID, _LastInteraction, NNr} | _T], ClientID) ->
+    NNr;
+getClientNNrInternal([_H | T], ClientID) ->
+    getClientNNrInternal(T, ClientID).
+
+listCMEM({_RemTime, ClientList}) ->
+    listCMEMinternal(ClientList).
+
+listCMEMinternal([]) ->
+    [];
+listCMEMinternal([{ClientID, _LastInteraction, NNr} | T]) ->
+    [{ClientID, NNr} | listCMEMinternal(T)].
+
+lengthCMEM({_RemTime, ClientList}) ->
+    length(ClientList).
