cf7ac8dd055e5758d02c49897b1625d1d5823f7f
infwps562 <benjamin.schroeder@haw-hamburg.de>
2023-05-02 14:25:53 +0200

    add new client progress

--- src/praktikum1/client.erl
+++ src/praktikum1/client.erl
@@ -1,9 +1,9 @@
 -module(client).
 
--export([start/0, randomizeDelay/1]).
+-export([start/0, updateReadMsgMEM/2]).
 
 -import(util, [logging/2, writelist/2, randomliste/3]).
--import(vsutil, [get_config_value/2, now2string/1, now2stringD/1, meinSleep/1]).
+-import(vsutil, [get_config_value/2, now2string/1, now2stringD/1, meinSleep/1, validTS/1, diffTS/2]).
 -import(io_lib, [format/2]).
 
 start() ->
@@ -14,31 +14,33 @@ start() ->
     {ok, SendeIntervall} = get_config_value(sendeintervall, ClientConfig),
     {ok, ServerName} = get_config_value(servername, ClientConfig),
     {ok, ServerNode} = get_config_value(servernode, ClientConfig),
-    
+
     Server = {ServerName, ServerNode},
 
     startClients(Clients, LifeTime, SendeIntervall, Server, Clients),
 
     case net_adm:ping(ServerNode) of
-        pang -> nok; % Server Node konnte nicht gefunden werden
-        pong -> startClients(Clients, LifeTime, SendeIntervall, Server, Clients)
+        % Server Node konnte nicht gefunden werden
+        pang ->
+            nok;
+        pong ->
+            erlang:send_after(LifeTime * 1000, self(), {terminateClient}),
+            startClients(Clients, LifeTime, SendeIntervall, Server, Clients)
     end.
-
 
 startClients(_Clients, _LifeTime, _SendeIntervall, _Server, 0) ->
     ok;
-
 startClients(Clients, LifeTime, SendeIntervall, Server, Counter) ->
     {IntervalMin, IntervalMax} = SendeIntervall,
     [Delay] = randomliste(1, IntervalMin, IntervalMax),
     {ok, HostName} = inet:gethostname(),
-    ClientName = format("Client~p@~s210", [Clients - Counter, HostName]), % Gruppe 2 Team 10
+    % Gruppe 2 Team 10
+    ClientName = format("Client~p@~s210", [Clients - Counter, HostName]),
     LogFile = format("~s.log", [ClientName]),
     spawn(fun() -> loop([], LifeTime, Delay, Server, ClientName, LogFile) end),
     startClients(Clients, LifeTime, SendeIntervall, Server, Counter - 1).
 
-
-% TODO: terminieren nach LifeTime Sekunden + Testing
+% TODO: Testing
 loop(RMEM, LifeTime, Delay, Server, ClientName, LogFile) ->
     logging(LogFile, format("~p, ~p, ~p, ~p~n", [LifeTime, Delay, Server, ClientName])),
     redakteur(Delay, Server, ClientName, LogFile),
@@ -46,45 +48,141 @@ loop(RMEM, LifeTime, Delay, Server, ClientName, LogFile) ->
     NewDelay = randomizeDelay(Delay),
     loop(NewRMEM, LifeTime, NewDelay, Server, ClientName, LogFile).
 
-
 redakteur(Delay, Server, ClientName, LogFile) ->
-    sendLoop(Delay, Server, ClientName, LogFile, 5),
-    NNr = getNNr(Server, LogFile),
+    redakteurLoop(Delay, Server, ClientName, LogFile, 5),
+    NNr = getNNr(Server, ClientName, LogFile),
     Now = now2string(erlang:timestamp()),
     logging(LogFile, format("~pte_Nachricht um ~p|vergessen zu senden ******", [NNr, Now])).
 
-
-sendLoop(_Delay, _Server, _ClientName, _LogFile, 0) ->
+redakteurLoop(_Delay, _Server, _ClientName, _LogFile, 0) ->
     ok;
-
-sendLoop(Delay, Server, ClientName, LogFile, Counter) ->
-    NNr = getNNr(Server, LogFile),
+redakteurLoop(Delay, Server, ClientName, LogFile, Counter) ->
+    NNr = getNNr(Server, ClientName, LogFile),
     TSclientout = erlang:timestamp(),
     Msg = format("~s: ~pte_Nachricht. C Out: ~p~n", [ClientName, NNr, now2string(TSclientout)]),
     Server ! {dropmessage, [NNr, Msg, TSclientout]},
     meinSleep(Delay * 1000),
-    sendLoop(Delay, Server, ClientName, LogFile, Counter - 1).
+    redakteurLoop(Delay, Server, ClientName, LogFile, Counter - 1).
 
-
-% TODO fix unsafe receive
-getNNr(Server, LogFile) ->
+getNNr(Server, ClientName, LogFile) ->
     Server ! {self(), getmsgid},
-    NNr = receive
-        {nid, NNr} -> NNr;
-        Answer -> logging(LogFile, format("Unexpected answer from server: ~p~n", [Answer]))
-    end,
-    NNr.
-
+    receive
+        {nid, NNr} ->
+            NNr;
+        {terminateClient} ->
+            logging(LogFile, format("~p nach Ablauf seiner Lifetime terminiert.~n", [ClientName]))
+    after 3000 ->
+        logging(
+            LogFile,
+            format("~p aufgrund eines Fehlers terminiert. Keine Antwort vom Server erhalten.~n", [
+                ClientName
+            ])
+        ),
+        exit(normal)
+    end.
 
 randomizeDelay(Delay) ->
     [Random] = randomliste(1, 0, 1),
-    Return = if (0.5 * Delay) < 2 -> Delay * 1.5;
-            true -> if Random == 1 -> Delay * 1.5;
+    Return =
+        if
+            (0.5 * Delay) < 2 ->
+                Delay * 1.5;
+            true ->
+                if
+                    Random == 1 -> Delay * 1.5;
                     true -> Delay * 0.5
-                    end
-            end,
+                end
+        end,
     Return.
 
+% TODO: Leser
+leser(RMEM, Server, ClientName, LogFile) ->
+    leserLoop(RMEM, Server, ClientName, LogFile),
+    RMEM.
 
-% TODO Leser + ReadMsgMEM
-leser(RMEM, Server, ClientName, LogFile) -> RMEM.
+
+leserLoop(RMEM, Server, ClientName, LogFile) ->
+    {Message, Terminated} = getNewMessage(Server, ClientName, LogFile),
+    [NNr, Msg, TSclientout, TShbqin, _TSdlqin, TSdlqout] = Message,
+    TSclientin = erlang:timestamp(),
+    {Repetition, Amount, NewRMEM} = updateReadMsgMEM(RMEM, NNr),
+    MsgString = if Repetition -> % andere Schritte überspringen, wenn Nachricht zum wiederholten Mal empfangen wird
+                        format(">>>Wiederholung<<<: Nummer ~p zum ~p-ten mal erhalten.", [NNr, Amount]);
+                true ->
+                    IsEigenerRedakteur = isEigenerRedakteur(Msg, ClientName),
+                    NewMsg =    if IsEigenerRedakteur -> Msg ++ "*******";
+                                true -> Msg
+                                end,
+                    checkFuture(NewMsg, TSclientout, TShbqin, TSdlqout, TSclientin, LogFile, NNr)
+                end,
+    logging(LogFile, format("~s ; C In: ~s", [MsgString, now2string(TSclientin)])),
+    if Terminated ->
+        leserLoop(NewRMEM, Server, ClientName, LogFile);
+    true ->
+        logging(LogFile, "Leser terminiert~n")
+    end.
+                    
+
+
+getNewMessage(Server, ClientName, LogFile) ->
+    Server ! {self(), getmessages},
+    receive
+        {reply, Message, Terminated} -> {Message, Terminated};
+        {terminateClient} -> logging(LogFile, format("~p nach Ablauf seiner Lifetime terminiert.~n", [ClientName]))
+    after 3000 ->
+        logging(LogFile, format("~p aufgrund eines Fehlers terminiert. Keine Antwort vom Server erhalten.~n", [ClientName])),
+        exit(normal)
+    end.
+
+
+% TODO: properly implement
+isEigenerRedakteur(_MsgString, _ClientName) ->
+    true.
+
+checkFuture(MsgString, TSclientout, TShbqin, TSdlqout, TSclientin, LogFile, NNr) ->
+    case validateTimestamps(TSclientout, TShbqin, TSdlqout, TSclientin) of
+        {false, false, _, _} ->
+            logging(LogFile, format("Nachricht #~p: Ungueltige Zeitstempel. Ueberpruefung fuer Nachricht aus "
+            "der Zukunft beim Server kann nicht durchgeführt werden.", [NNr])),
+            MsgString;
+        {_, _, false, false} ->
+            logging(LogFile, format("Nachricht #~p: Ungueltige Zeitstempel. Ueberpruefung fuer Nachricht aus "
+            "der Zukunft beim Leser kann nicht durchgeführt werden.", [NNr])),
+            MsgString;
+        _AllTrue ->
+            TimeDifferenceServer = diffTS(TShbqin, TSclientout),
+            TimeDifferenceLeser = diffTS(TSclientin, TSdlqout),
+            ZukunftServer = ">**Nachricht aus der Zukunft fuer Server:",
+            ZukunftLeser = ">**Nachricht aus der Zukunft fuer Leser:",
+            NewMsgString =  if TimeDifferenceServer < 0 ->
+                                MsgString ++ ZukunftServer ++ now2string(TimeDifferenceServer);
+                            true -> MsgString
+                            end,
+            if TimeDifferenceLeser < 0 ->
+                NewMsgString ++ ZukunftLeser ++ now2string(TimeDifferenceLeser);
+            true -> NewMsgString
+            end
+    end.
+
+
+validateTimestamps(TS1, TS2, TS3, TS4) ->
+    {validTS(TS1), validTS(TS2), validTS(TS3), validTS(TS4)}.
+
+updateReadMsgMEM(RMEM, NNr) ->
+    {Bool, Amount} = readFromRMEM(RMEM, NNr),
+    NewRMEM = updateRMEM(RMEM, NNr),
+    {Bool, Amount, NewRMEM}.
+
+updateRMEM([], NNr) ->
+    [{NNr, 1}];
+updateRMEM([{NNr, Amount} | T], NNr) ->
+    [{NNr, Amount + 1} | T];
+updateRMEM([H | T], NNr) ->
+    [H | updateRMEM(T, NNr)].
+
+readFromRMEM([], _NNr) ->
+    {false, 0};
+readFromRMEM([{NNr, Amount} | _T], NNr) ->
+    {true, Amount};
+readFromRMEM([_H | T], NNr) ->
+    readFromRMEM(T, NNr).
