5e1340e5ea8902f15a4ac188e9bfc431d0acf4e4
antoni <jedth@veril.mozmail.com>
2023-05-04 11:27:38 +0200

    update Entwurf numbering in dlq, update lengthDLQ

--- src/praktikum1/dlq.erl
+++ src/praktikum1/dlq.erl
@@ -20,16 +20,21 @@ initDLQ(DLQLimit, LogFile) ->
     % 1 und 3
     {DLQLimit, []}.
 
+%% @doc Löscht die DLQ.
 delDLQ(_Queue) ->
+    % 1, 2
     ok.
 
 %% @doc Ermittelt die als nächstes erwartete Nachrichennummer der DLQ.
+%% @end
+%%                     1
 expectedNr({_Capacity, []}) ->
+    % 2
     1;
 expectedNr({_Capacity, Queue}) ->
-    % 1
+    % 3
     [[NNr | _MessageTail] | _QueueTail] = reverse(Queue),
-    % io:format("NNr = ~p~n", [NNr]),
+    % 4
     NNr + 1.
 
 %% @doc Verschiebt eine Nachricht in die DLQ und ergänzt diese um einen Zeitstempel.
@@ -40,17 +45,16 @@ push2DLQ(Message, Queue = {Capacity, Messages}, LogFile) ->
         case length(Message) > 2 of
             true ->
                 [NNr, Msg, TSclientout, TShbqin] = Message,
+                %                      2                                  1
                 [NNr, Msg ++ now2string(TSdlqin), TSclientout, TShbqin, TSdlqin];
             false ->
                 [NNr, Msg] = Message,
-                [NNr, Msg ++ now2string(TSdlqin)]
+                %                      2           1
+                [NNr, Msg ++ now2string(TSdlqin), TSdlqin]
         end,
 
-    % 7
-    logging(LogFile, io_lib:format("DLQ: ~p wurde in die DLQ eingefügt.~n", [NewMsg])),
-
-    %               3                 4
     NewQueue =
+        %       3                    4
         case length(Messages) =< lengthDLQ(Queue) of
             true ->
                 % 6
@@ -61,6 +65,10 @@ push2DLQ(Message, Queue = {Capacity, Messages}, LogFile) ->
                 % 6
                 add2dlq(NewMsg, {Capacity, T})
         end,
+
+    % 7
+    logging(LogFile, io_lib:format("DLQ: ~p wurde in die DLQ eingefügt.~n", [NewMsg])),
+
     % 8
     NewQueue.
 
@@ -96,67 +104,79 @@ add2dlq_intern(Elem = [ElemNNr | _ElemTail],
 %%
 %% Anschließend wird die tatsächlich versendete Nachrichtennummer zurückgegeben.
 deliverMSG(MSGNr, ClientPID, Queue = {_Capacity, Messages}, LogFile) ->
+    % 1
     ExpectedNNr = expectedNr(Queue),
+    %                         2
     case getMessageByNr(MSGNr, Messages, LogFile) of
+        % 3        4       5
         nok when MSGNr + 1 < ExpectedNNr ->
             deliverMSG(MSGNr + 1, ClientPID, Queue, LogFile);
         nok ->
+            % 14
             NewMessage = [-1, nokA, 0, 0, 0, 0],
+            % 13
             logging(LogFile,
                     io_lib:format("DLQ: Nachricht=~B konnte nicht gefunden werden. ~p wurde an "
                                   "~p gesendet.",
                                   [MSGNr, NewMessage, ClientPID])),
+            %          16                   15
             ClientPID ! {reply, NewMessage, true},
+            % 17
             -1;
         Message ->
             Terminated =
+                % 6
                 if MSGNr == ExpectedNNr - 1 ->
-                       true;
+                       true; % 7
                    true ->
-                       false
+                       false % 8
                 end,
             TSdlqout = erlang:timestamp(),
             [NNr, Msg, TSclientout, TShbqin, TSdlqin] = Message,
+            % 9
             NewMessage = [NNr, Msg ++ now2string(TSdlqout), TSclientout, TShbqin, TSdlqin, TSdlqout],
+            % 11
             ClientPID ! {reply, NewMessage, Terminated},
+            % 10
             logging(LogFile,
                     io_lib:format("DLQ: ~p wurde an ~p gesendet.~n", [NewMessage, ClientPID])),
+            % 12
             MSGNr
     end.
 
 %% @doc Gibt eine Liste aller Nachrichtennummern in der DLQ zurück.
-
-% 1, 2
+%% @end
+%%                  1
 listDLQ({_Capacity, List}) ->
     listDLQintern(List).
 
-%% 3
+%%           2, 3
 listDLQintern([]) ->
     [];
 %%               5               4
 listDLQintern([[CurrentNNr | _T] | Tail]) ->
     %    6                7
-    % 8
-    [CurrentNNr | listDLQintern(Tail)].
+    [CurrentNNr | listDLQintern(Tail)]. % 8
 
-%% @doc Gibt die Kapazitaet der DLQ zurueck.
-lengthDLQ({Capacity, _Queue}) ->
-    Capacity.
+%% @doc Gibt die momentane Länge der DLQ zurueck.
+lengthDLQ({_Capacity, Queue}) ->
+    length(Queue). % 1,2
 
 %% -------------------------------------
 %% Util
 %% -------------------------------------
 
 %% @doc Gibt bei Erfolg die zugehörige Nachricht zu einer angegebenen Nachrichtennummer zurück.
-%%                  2
+%% @end
+%%                 1, 2
 getMessageByNr(NNr, [], LogFile) ->
     logging(LogFile, io_lib:format("NNr=~B wurde nicht in DLQ gefunden.~n", [NNr])),
     nok;
-%%                             3, 5
+%%                    3          5
 getMessageByNr(NNr, [Message = [NNr | _Tail]|_QueueTail], _LogFile) ->
     % 7
     Message;
-%%                           3, 5
+%%                      3, 5
 getMessageByNr(NNr, [_CurrentMsg | Tail], LogFile) ->
     %                    6
     getMessageByNr(NNr, Tail, LogFile).
