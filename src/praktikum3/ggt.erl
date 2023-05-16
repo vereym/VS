12097345dae4105b0d432334bfb2d482b4492bc2
infwps562 <benjamin.schroeder@haw-hamburg.de>
2023-05-16 15:42:18 +0200

    add ggt progress

--- src/praktikum3/ggt.erl
+++ src/praktikum3/ggt.erl
@@ -1,29 +1,59 @@
 -module(ggt).
 -export([start/8]).
+-import(util, [logging/2, randomliste/3]).
+-import(vsutil, [get_config_value/2, now2string/1]).
+-import(io_lib, [format/2]).
+
+% TODO: 17-23
 
 start(Delay, TermZeit, GGTNum, StarterNum, Gruppe, Team, NameService, Koordinator) ->
-    pass.
-
-loop() ->
+    % 15
+    GGTName = list_to_atom(format('~B~B~B~B', [Gruppe, Team, GGTNum, StarterNum])),
+    {ok, HostName} = inet:gethostname(),
+    LogFile = format("GGT-~s@~s", [GGTName, HostName]),
+    % 16
+    register(GGTName, self()),
+    NameService ! {self(), {rebind, GGTName, node()}},
     receive
-        {setneighbors, LeftN, RightN} ->
-            loop();
+        ok_overwrite ->
+            logging(LogFile, format("Erneut beim Namensdienst registriert!~n", []));
+        ok_new ->
+            logging(LogFile, format("Erfolgreich beim Namensdienst registriert!~n", []))
+    end,
+    Koordinator ! {self(), hello},
+    Neighbors =
+        receive
+            {setneighbors, LeftN, RightN} ->
+                {LeftN, RightN}
+        end,
+    receive
+        % 17
         {setpm, MiNeu} ->
-            loop();
+            loop([Delay, TermZeit, GGTName, NameService, Koordinator, Neighbors], MiNeu)
+    end.
+
+loop(Constants = [Delay, TermZeit, GGTName, NameService, Koordinator, Neighbors], Mi) ->
+    receive
+        % 17
+        {setpm, MiNeu} ->
+            loop(Constants, MiNeu);
         {sendy, Y} ->
-            loop();
+            loop(Constants, Mi);
         {calc, start} ->
-            loop();
+            loop(Constants, Mi);
         {From, {vote, Initiator, MiIn}} ->
-            loop();
+            loop(Constants, Mi);
         {voteYes, Name} ->
-            loop();
+            loop(Constants, Mi);
         {From, toggle} ->
-            loop();
+            loop(Constants, Mi);
         {From, tellmi} ->
-            loop();
+            loop(Constants, Mi);
         {From, pingGGT} ->
-            loop();
+            loop(Constants, Mi);
         kill ->
-            loop()
+            ok
+    after TermZeit * 1000 ->
+        % TODO properly terminate
+        pass
     end.
