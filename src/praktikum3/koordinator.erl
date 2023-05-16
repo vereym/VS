9b3f5ac1093f87480bb79c5eb3e5591d87fc11ab
antoni <jedth@veril.mozmail.com>
2023-05-16 10:58:27 +0200

    start implementing koordinator

--- src/praktikum3/koordinator.erl
+++ src/praktikum3/koordinator.erl
@@ -2,22 +2,48 @@
 
 -export([start/0]).
 
+-import(vsutil, [get_config_value/2, now2string/1, now2stringD/1]).
+-import(util, [logging/2]).
+-import(io_lib, [format/2, format/1]).
+
 start() ->
-    {ok, GGTConfig} = file:consult("koordinator.cfg"),
-    {ok, KoordinatorName} = get_config_value(koordinatorname, GGTConfig), % TODO
-    {ok, KoordinatorNode} = get_config_value(koordinatornode, GGTConfig), % TODO
-    {ok, GGTNode} = get_config_value(ggtnode, GGTConfig), % TODO
-    spawn(fun() -> loop([]) end),
+    {ok, HostName} = inet:gethostname(),
+    LogFile = format("koordinator@~s", [HostName]),
+
+    %% config auslesen
+    {ok, KoordinatorConfig} = file:consult("koordinator.cfg"),
+    {ok, NameServiceNode} = get_config_value(nameservicenode, KoordinatorConfig),
+    {ok, GGTProzessanzahl} = get_config_value(ggtprozessanzahl, KoordinatorConfig),
+    {ok, TermZeit} = get_config_value(arbeitszeit, KoordinatorConfig),
+    {ok, KoordinatorNode} = get_config_value(termzeit, KoordinatorConfig),
+    {ok, Koorigieren} = get_config_value(korrigieren, KoordinatorConfig),
+
+    NameService =
+        case net_adm:ping(NameServiceNode) of
+            pang ->
+                logging(LogFile, format("nameservice konnte nicht gefunden werden~n"));
+            pong ->
+                global:whereis_name(nameservice)
+        end,
+
+    spawn(fun() ->
+             loop([NameService, GGTProzessanzahl, TermZeit, KoordinatorNode, Koorigieren, LogFile])
+          end),
     ok.
 
-loop(X) ->
+loop(X = [NameService,
+          GGTProzessanzahl,
+          TermZeit,
+          KoordinatorNode,
+          Koorigieren,
+          LogFile]) ->
     receive
         {From, getsteeringval} ->
-            %% AZMin, AZMax = simulierte Verzögerungszeit zur Berechnung in Sekunden
+            %% AZMin, AZMax = simulierte Verzögerungszeit zur Berechnung in Sekunden %% TODO
             %% TermZeit = Wartezeit in Sekunden, bis eine Wahl für eine Terminierung initiiert wird
             %% GGTProzessnummer = Anzahl zu startender ggT-Prozesse
-            From ! {steeringval, {AZMin = 1, AZMax = 3}, TermZeit = 1, Anzahl = 1},
-            ok,
+            From ! Msg = {steeringval, {AZMin = 1, AZMax = 3}, TermZeit, GGTProzessanzahl},
+            logging(LogFile, format("~p an ~p geschickt~n", [Msg, From])),
             loop(X);
         {hello, Clientname} ->
             ok,
